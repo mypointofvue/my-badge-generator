ba8a420c0737fffde16452eee33902e0ff17a134
lordvue <83044037+lordvue@users.noreply.github.com>
2022-05-18 09:33:02 -0500

    added .strip to f in jacocoFileList to clean white spaces

--- src/JacocoBadgeGenerator.py
+++ src/JacocoBadgeGenerator.py
@@ -255,8 +255,11 @@ def filterMissingReports(jacocoFileList, failIfMissing=False) :
     don't exist, then it will exit with a non-zero exit code causing
     workflow to fail.
     """
+    print ("jacocoFileList" + jacocoFileList)
     goodReports = []
     for f in jacocoFileList :
+        f = f.strip()
+        print("os path function result:" + os.path.exists(f))
         if os.path.exists(f) :
             goodReports.append(f)
         else :
